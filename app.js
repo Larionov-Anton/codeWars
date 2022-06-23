bbae7864f9a0314ab65d1dc079af68ba4889e10c
Larionov-Anton <larionovanton05@gmail.com>
2022-06-23 11:51:27 +0300

    Add 2 numbers
    convert the sum to a binary system as a string

---
+++ app.js
@@ -0,0 +1,12 @@
+// *Сложить 2 числа, преобразовать сумму в двоичную систему в виде строки
+
+/*
+function addBinary(a,b) {
+	let summ = a + b;
+	return summ.toString(2);
+ }
+ console.log(addBinary(2,2))
+*/
+ 
+
+// *
