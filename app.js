adf2f370f650df2c3dabce53a40ff28448dcefc4
Larionov-anton <larionovanton05@gmail.com>
2023-06-20 10:11:41 +0300

    recursion

--- app.js
+++ app.js
@@ -1,42 +1,56 @@
-// *Сложить 2 числа, преобразовать сумму в двоичную систему в виде строки
-
+// * Функция rgb не завершена. Завершите его так, чтобы передача десятичных значений RGB привела к возвращению шестнадцатеричного представления. Допустимые десятичные значения для RGB - 0-255. Любые значения, выходящие за пределы этого диапазона, должны быть округлены до ближайшего допустимого значения. Примечание: Ваш ответ всегда должен состоять из 6 символов, сокращение с 3 здесь не сработает.
 /*
-function addBinary(a,b) {
-	let summ = a + b;
-	return summ.toString(2);
- }
- console.log(addBinary(2,2))
-*/
- 
+function rgb(...args) {
+  const validateArgs = args.map((item) => {
+    if (item <= 0) {
+      return (item = 0).toString(16);
+    } else if (item > 255) {
+      return (item = 255).toString(16);
+    } else return item.toString(16);
+  });
+  const transformArgs = validateArgs.map((item) => {
+    if (item.length < 2) return "0" + item;
+    else return item;
+  });
 
-// * Преобразовать массив так что бы каждое число в массиве повторялось только N количество раз без изменения порядка 
+  return transformArgs.join("").toUpperCase();
+}
 
-// Алгоритм - фильтровать массив циклом и составить новый массив из элементов повторябщихся N количество раз
+// Оптимизированное 
+const rgb = (r, g, b) =>
+  [r, g, b].map(val => Math.max(0, Math.min(255, val)).toString(16).padStart(2, `0`)).join(``).toUpperCase();
 
-/*
-function deleteNth(arr, x) {
-	let totals = {};
-	let newArr = arr.filter(o => (totals[o] = ++totals[o] || 1) <= x);
+	*/
 
-	return newArr;	
- }
- */
- 
-// * Есть целое число m - общий объем здания. Вычислить на основе этого числа количество кубов которые нужно построить, если первый куб 1^3 и каждый последующий (n+1)^3. Вернуть целое число количества кубов или -1 если такого числа нет.
+//*	Завершите решение так, чтобы функция разбила camelcase на части, используя пробел между словами.
 
-/*
-function findNb(m) {
- 
-	for (let n = 0; ; n++) {
-		if (m > 0) {
-			let firsCube = Math.pow( n + 1,3 );
-			m = m - firsCube;
-		} else if (m == 0) {
-			return n;
-		} else 
-			return -1;
-	}	
- }
+// function solution(string) {
+//   return string.replace(/[A-Z]/g, " $&");
+// }
 
- console.log(findNb(4183059834009));
-*/
+// solution("helloWorld");
+
+// * Напишите функцию, которая принимает строку фигурных скобок и определяет, является ли порядок фигурных скобок допустимым. Она должна возвращать, true если строка допустима, и false если она недопустима.
+
+// const braces = "(){}[]"
+
+// function validBraces(braces){
+// 	let opening = [ '(', '[', '{']
+// 	let closing = [ ')', ']', '}']
+// 	let stack = []
+// 	//console.log(closing.indexOf(braces[")"]) === opening.indexOf(arr[")"]))
+// 	for (let i = 0; i < braces.length; i++) {
+// 			const brace = braces[i]
+// 			if (opening.includes(brace)) {
+// 				stack.push(brace)
+// 			} else
+// 			if (closing.indexOf(brace) === opening.indexOf(stack[stack.length - 1])) {
+// 				stack.pop()
+// 			} else return false
+// 	} return stack.length === 0;
+// }
+
+// function validBraces(braces){
+//   let re = /\(\)|\{\}|\[\]/;
+//   return re.test(braces) ? validBraces(braces.replace(re, '')) : '' === braces;
+// }
